8bf2a000beb6bcf51a740905259e2d906a0efc7d
wfarlessyost <wfarless@unca.edu>
2020-03-17 20:32:30 -0400

    Updated Gazebo world for link attachment

---
+++ build/pick_place/CMakeFiles/pick_place.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pick_place.dir/src/full_robot_pick_place.cpp.o"
+  "/home/will/ur3_ws/devel/lib/pick_place/pick_place.pdb"
+  "/home/will/ur3_ws/devel/lib/pick_place/pick_place"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pick_place.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
